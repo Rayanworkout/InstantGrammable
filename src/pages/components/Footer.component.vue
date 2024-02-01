c2e74d6d8f030c35e102fb116528788ca4e4643b
rayan <rayan13170@protonmail.com>
2024-02-01 21:02:41 +0100

    added opening hours

--- src/pages/components/Footer.component.vue
+++ src/pages/components/Footer.component.vue
@@ -21,12 +21,14 @@
                         <div class="col-sm-6 text-center text-sm-right border-right mb-3 mb-sm-0">
                             <h5 class="font-weight-bold mb-2">Bienvenue !</h5>
                             <p class="mb-2">Rue de la Reine Jeanne, 04100 Manosque</p>
-                            <p class="mb-0">06.01.01.01.01</p>
+                            <p class="mb-0">06.77.00.54.76</p>
                         </div>
                         <div class="col-sm-6 text-center text-sm-left">
                             <h5 class="font-weight-bold mb-2">Horaires d'ouverture</h5>
-                            <p class="mb-2">Lundi – Samedi, 8h – 17h</p>
-                            <p class="mb-0">Dimanche: Fermé</p>
+                            <p class="mb-2">Lundi: Fermé</p>
+                            <p class="mb-2">Mardi à Jeudi, 10h – 15h & 18h - 21h30</p>
+                            <p class="mb-2">Vendredi & Samedi, 10h – 15h & 18h - 23h</p>
+                            <p class="mb-0">Dimanche, 10h - 15h</p>
                         </div>
                     </div>
                 </div>
